451f78826a754185dec41fda2f380bb40ca74257
habul17 <abulhassanca.dev@gmail.com>
2026-07-01 09:39:25 +0530

    Create Backend

--- index.js
+++ index.js
@@ -2,10 +2,33 @@ const express = require('express');
 
 const app = express();
 
-const NOTES = [];
+app.use(express.json()); // Parse Json Body
 
-app.post("notes", (req, res) => {
+const NOTES = []; // In-Memory variable to store notes
+
+
+// POST - Create Notes
+
+app.post("/notes", (req, res) => {
     
+    const note = req.body.note
+
+    NOTES.push(note);
+
+    res.json({
+        message : "Note Created Successfully"
+    })
 })
+
+
+// GET - Get all Notes
+
+app.get("/notes", (req, res) => {
+
+    res.json({
+        NOTES
+    })
+})
+
 
 app.listen(3000);
